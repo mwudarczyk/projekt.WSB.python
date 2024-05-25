06dcef5fe1b123b03d61ba377f419c35d4e9f324
mwudarczyk <151441700+mwudarczyk@users.noreply.github.com>
2024-05-25 21:20:17 +0200

    Add files via upload

--- project.WSB.py
+++ project.WSB.py
@@ -20,15 +20,20 @@ heading.place(x=30, y=5)
 user = Entry(frame, width=25, fg='#57a1f8', border=0, bg='white', font=('72 Condensed',12))
 user.place(x=40, y=85)
 user.insert(0, 'Nazwa użytkownika')
-Frame(frame, width=295, height=2, bg='#57a1f8').place(x=35,y=110)
+Frame(frame, width=295, height=3, bg='teal').place(x=35,y=110)
 
 #------------password----------------
 password = Entry(frame, width=25, fg='#57a1f8', border=0, bg='white', font=('72 Condensed',12))
 password.place(x=40, y=155)
 password.insert(0, 'Hasło')
-Frame(frame, width=295, height=2, bg='#57a1f8').place(x=35,y=180)
+Frame(frame, width=295, height=3, bg='teal').place(x=35,y=180)
 
 Button(frame, width=40, pady=8, text='Wszystko gotowe. Zaloguj', bg='#57a1f8', fg='white', border=0).place(x=40, y=200)
+label=Label(frame,text='Nie masz jeszcze konta?', fg='#57a1f8', bg='white', font=('72 Condensed',10))
+label.place(x=80, y=260)
+
+sign_up= Button(frame, width=14, text='kliknij i działamy!', border=0, bg='white', cursor='hand2', fg='teal')
+sign_up.place(x=200, y=260)
 
 root.mainloop()
 

aed97c264740c8c71c9f76a6a56b2ceacb90fc73
mwudarczyk <151441700+mwudarczyk@users.noreply.github.com>
2024-05-25 22:16:14 +0200

    Add files via upload

--- project.WSB.py
+++ project.WSB.py
@@ -7,32 +7,68 @@ root.geometry("900x500+200+100")
 root.configure(bg='#fff')
 root.resizable(False, True)
 
+def signin():
+    username = user.get()
+    password = passw.get()
+
+    if username == 'kurspython2024' and password == 'wsb':
+        screen=Toplevel(root)
+        screen.title('WITAM CIĘ')
+        screen.geometry('900x500+200+100')
+        screen.config(bg='white')
+        Label(screen, text='Witaj na oficjalnej stronie\nWU.STUDIO!',bg='#fff',fg='blue', font=('Bauhaus 93', 36)).pack(expand=True)
+        screen.mainloop()
+        print('correct')
+
 img = PhotoImage(file='login.png')
 Label(root, image=img, bg='white').place(x=60, y=60)
 
 frame=Frame(root, width=350, height=350, bg='white')
 frame.place(x=500, y=70)
 
-heading=Label(frame, text='Zaloguj do serwisu',fg='#57a1f8', bg='white', font=('Bauhaus 93',26))
+heading=Label(frame, text='Zaloguj do serwisu',fg='blue', bg='white', font=('Bauhaus 93',26))
 heading.place(x=30, y=5)
 
 #------------username----------------
-user = Entry(frame, width=25, fg='#57a1f8', border=0, bg='white', font=('72 Condensed',12))
+def enter(n):
+    user.delete(0, 'end')
+
+def leave(n):
+    name=user.get()
+    if name == '':
+        user.insert(0, 'Nazwa użytkownika')
+
+
+user = Entry(frame, width=25, fg='blue', border=0, bg='white', font=('72 Condensed',12))
 user.place(x=40, y=85)
 user.insert(0, 'Nazwa użytkownika')
-Frame(frame, width=295, height=3, bg='teal').place(x=35,y=110)
+user.bind('<FocusIn>', enter)
+user.bind('<FocusOut>', leave)
+Frame(frame, width=295, height=3, bg='tan').place(x=35,y=110)
 
 #------------password----------------
-password = Entry(frame, width=25, fg='#57a1f8', border=0, bg='white', font=('72 Condensed',12))
-password.place(x=40, y=155)
-password.insert(0, 'Hasło')
-Frame(frame, width=295, height=3, bg='teal').place(x=35,y=180)
+def enter(n):
+    passw.delete(0, 'end')
 
-Button(frame, width=40, pady=8, text='Wszystko gotowe. Zaloguj', bg='#57a1f8', fg='white', border=0).place(x=40, y=200)
-label=Label(frame,text='Nie masz jeszcze konta?', fg='#57a1f8', bg='white', font=('72 Condensed',10))
+def leave(n):
+    name=passw.get()
+    if name == '':
+        passw.insert(0, 'Hasło')
+
+
+passw = Entry(frame, width=25, fg='blue', border=0, bg='white', font=('72 Condensed',12))
+passw.place(x=40, y=155)
+passw.insert(0, 'Hasło')
+passw.bind('<FocusIn>', enter)
+passw.bind('<FocusOut>', leave)
+Frame(frame, width=295, height=3, bg='tan').place(x=35,y=180)
+
+#------------login/sign up-----------
+Button(frame, width=40, pady=8, text='Wszystko gotowe. Zaloguj', bg='blue', fg='white', border=0, command=signin).place(x=40, y=200)
+label=Label(frame,text='Nie masz jeszcze konta?', fg='blue', bg='white', font=('72 Condensed',10))
 label.place(x=80, y=260)
 
-sign_up= Button(frame, width=14, text='kliknij i działamy!', border=0, bg='white', cursor='hand2', fg='teal')
+sign_up= Button(frame, width=14, text='kliknij i działamy!', border=0, bg='white', cursor='hand2', fg='tan')
 sign_up.place(x=200, y=260)
 
 root.mainloop()

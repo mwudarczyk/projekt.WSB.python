087007049f06ef4b44c569a45815457e5e6fa203
mwudarczyk <151441700+mwudarczyk@users.noreply.github.com>
2024-05-25 20:58:54 +0200

    Add files via upload

--- project.WSB.py
+++ project.WSB.py
@@ -3,9 +3,9 @@ from tkinter import messagebox
 
 root=Tk()
 root.title("Panel logowania/tworzenia konta")
-root.geometry("900x600+200+100")
+root.geometry("900x500+200+100")
 root.configure(bg='#fff')
-root.resizable(True, True)
+root.resizable(False, True)
 
 img = PhotoImage(file='login.png')
 Label(root, image=img, bg='white').place(x=60, y=60)
@@ -16,8 +16,19 @@ frame.place(x=500, y=70)
 heading=Label(frame, text='Zaloguj do serwisu',fg='#57a1f8', bg='white', font=('Bauhaus 93',26))
 heading.place(x=30, y=5)
 
-user = Entry(frame, width=25, fg='black', border=2, bg='white', font=('Bauhaus 93',12))
-user.place(x=60, y=80)
+#------------username----------------
+user = Entry(frame, width=25, fg='#57a1f8', border=0, bg='white', font=('72 Condensed',12))
+user.place(x=40, y=85)
+user.insert(0, 'Nazwa użytkownika')
+Frame(frame, width=295, height=2, bg='#57a1f8').place(x=35,y=110)
+
+#------------password----------------
+password = Entry(frame, width=25, fg='#57a1f8', border=0, bg='white', font=('72 Condensed',12))
+password.place(x=40, y=155)
+password.insert(0, 'Hasło')
+Frame(frame, width=295, height=2, bg='#57a1f8').place(x=35,y=180)
+
+Button(frame, width=40, pady=8, text='Wszystko gotowe. Zaloguj', bg='#57a1f8', fg='white', border=0).place(x=40, y=200)
 
 root.mainloop()
 

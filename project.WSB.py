7f5f2eb355c07eac8a2799a46e1ad7e10b92a85b
mwudarczyk <151441700+mwudarczyk@users.noreply.github.com>
2024-05-25 20:07:03 +0200

    Add files via upload

---
+++ project.WSB.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+root.title("Zaloguj się")
+root.geometry("900x600+200+100")
+root.configure(bg='#fff')
+root.resizable(True, True)
+
+img = PhotoImage(file='login.png')
+Label(root, image=img, bg='white').place(x=60, y=60)
+
+root.mainloop()

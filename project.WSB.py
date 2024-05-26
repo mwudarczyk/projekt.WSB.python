3c364b99a5836cc3d1117ed18a53c8a5af8468f7
mwudarczyk <151441700+mwudarczyk@users.noreply.github.com>
2024-05-26 12:08:42 +0200

    Add files via upload

--- project.WSB.py
+++ project.WSB.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 root=Tk()
 root.title("Panel logowania/tworzenia konta")
 root.geometry("900x500+200+100")
-root.configure(bg='#fff')
+root.configure(bg='linen')
 root.resizable(False, True)
 
 def signin():
@@ -15,26 +15,26 @@ def signin():
         screen=Toplevel(root)
         screen.title('WITAJ W PRZESTRZENI WU.STUDIO')
         screen.geometry('900x500+200+100')
-        screen.config(bg='white')
-        Label(screen, text='Witaj na oficjalnej stronie\nWU.STUDIO!',bg='#fff',fg='blue', font=('Bauhaus 93', 36)).pack(expand=False)
+        screen.config(bg='linen')
+        Label(screen, text='Witaj na oficjalnej stronie\nWU.STUDIO!',bg='linen',fg='blue', font=('Bauhaus 93', 36)).pack(expand=False)
         screen.mainloop()
 
-    elif username!= 'kurspython2024' and password != 'wsb':
+    elif username != 'kurspython2024' and password != 'wsb':
         messagebox.showerror('BŁĄD', 'Niestety wygląda na to, że wpisałeś błędną nazwę użytkownika oraz hasło')
 
-    elif username!= 'kurspython2024':
+    elif username != 'kurspython2024':
         messagebox.showerror('BŁĄD', 'Niestety nazwa użytkownika jest niepoprawna')
 
-    elif passw!= 'wsb':
+    elif passw != 'wsb':
         messagebox.showerror('BŁĄD', 'Niestety hasło jest niepoprawne')
 
-img = PhotoImage(file='5-kopia 3.png')
-Label(root, image=img, bg='white').place(x=10, y=0)
 
-frame=Frame(root, width=360, height=400, bg='white')
+img = PhotoImage(file='5-kopia 3.png')
+Label(root, image=img, bg='linen').place(x=10, y=0)
+frame=Frame(root, width=360, height=400, bg='linen')
 frame.place(x=500, y=70)
 
-heading=Label(frame, text='Zaloguj do serwisu',fg='blue', bg='white', font=('Bauhaus 93',26))
+heading=Label(frame, text='Zaloguj do serwisu',fg='blue', bg='linen', font=('Bauhaus 93',26))
 heading.place(x=30, y=5)
 
 #------------username----------------
@@ -47,7 +47,7 @@ def leave(n):
         user.insert(0, 'Nazwa użytkownika')
 
 
-user = Entry(frame, width=25, fg='blue', border=0, bg='white', font=('72 Condensed',12))
+user = Entry(frame, width=25, fg='blue', border=0, bg='linen', font=('72 Condensed',12))
 user.place(x=40, y=85)
 user.insert(0, 'Nazwa użytkownika')
 user.bind('<FocusIn>', enter)
@@ -64,7 +64,7 @@ def leave(n):
         passw.insert(0, 'Hasło')
 
 
-passw = Entry(frame, width=25, fg='blue', border=0, bg='white', font=('72 Condensed',12))
+passw = Entry(frame, width=25, fg='blue', border=0, bg='linen', font=('72 Condensed',12))
 passw.place(x=40, y=155)
 passw.insert(0, 'Hasło')
 passw.bind('<FocusIn>', enter)
@@ -73,10 +73,10 @@ Frame(frame, width=295, height=2, bg='coral').place(x=35,y=180)
 
 #------------login/sign up-----------
 Button(frame, width=40, pady=8, text='Wszystko gotowe. Zaloguj', bg='blue', fg='white', border=0, font=('72 Condensed',12), command=signin).place(x=40, y=200)
-label=Label(frame,text='Nie masz jeszcze konta?', fg='blue', bg='white', font=('72 Condensed',10))
+label=Label(frame,text='Nie masz jeszcze konta?', fg='blue', bg='linen', font=('72 Condensed',10))
 label.place(x=80, y=260)
 
-sign_up= Button(frame, width=15, text='kliknij i lecimy!', border=0, bg='white', cursor='hand2', fg='coral', font=('Bauhaus 93', 10))
+sign_up= Button(frame, width=15, text='kliknij i lecimy!', border=0, bg='linen', cursor='hand2', fg='coral', font=('Bauhaus 93', 10))
 sign_up.place(x=200, y=260)
 
 root.mainloop()
